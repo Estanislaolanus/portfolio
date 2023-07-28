c6a8b1ebf5a42945f87bf85fc5985b4adf53ac1f
Lao <estanislaolanus@gmail.com>
2023-07-28 17:37:07 -0300

    Sass added

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,11 @@
 const hiddenElements = document.querySelectorAll(".hidden");
+const about = document.querySelector(".about");
 
-const tween = KUTE.fromTo(
-    '#blob1',
-    {path: "#blob1"},
-    {path: "#blob2"},
-    {repeat: 999, duration: 2000, yoyo: true}
+const tween = KUTE.allFromTo(
+    '.blob1',
+    {path: ".blob1"},
+    {path: ".blob2"},
+    {repeat: 1000, duration: 2500, yoyo: true}
 );
 tween.start();
 const observer = new IntersectionObserver(entries => {
@@ -12,7 +13,21 @@ const observer = new IntersectionObserver(entries => {
         if(entry.isIntersecting) {
             entry.target.classList.add("show");
         }
-    })
+    });
 });
 
+document.addEventListener("scroll", () => {
+        const navbar = document.querySelector("#navbar");
+        const navbarRect = navbar.getBoundingClientRect();
+        const aboutRect = about.getBoundingClientRect();
+        console.log(navbarRect.bottom, aboutRect.top, aboutRect.bottom,navbarRect.bottom >= aboutRect.top)
+        if(navbarRect.bottom >= aboutRect.top && navbarRect.bottom <= aboutRect.bottom) {
+            navbar.classList.add("bg-2");
+            navbar.classList.remove("bg-1");
+        } else {
+            navbar.classList.add("bg-1");
+            navbar.classList.remove("bg-2");
+        }
+
+})
 hiddenElements.forEach(el => observer.observe(el));
