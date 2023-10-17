c1b9123ff4855ada314e584fbb251103977cf37d
Lao <estanislaolanus@gmail.com>
2023-10-17 15:42:30 -0300

    tech stack edited

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
 const hiddenElements = document.querySelectorAll(".hidden");
+const techStack = document.querySelector("#tech-stack");
 const about = document.querySelector(".about");
 const hamburgerBtn = document.querySelector("#hamburger");
 const sidebar = document.querySelector("#sidebar");
@@ -8,15 +9,21 @@ const text = document.querySelectorAll(".user-data");
 // Blob animation
 const tween = KUTE.allFromTo(
     '.blob1',
-    {path: ".blob1"},
-    {path: ".blob2"},
-    {repeat: 1000, duration: 2500, yoyo: true}
+    { path: ".blob1" },
+    { path: ".blob2" },
+    { repeat: 1000, duration: 2500, yoyo: true }
 );
 tween.start();
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
-        if(entry.isIntersecting) {
+        if (entry.isIntersecting) {
             entry.target.classList.add("show");
+            if (entry.target.classList.contains("tech-stack")) {
+                const children = entry.target.children;
+                for (const child of children) {
+                    child.classList.add("show");
+                };
+            }
         }
     });
 });
@@ -26,7 +33,7 @@ document.addEventListener("scroll", () => {
     const navbar = document.querySelector("#navbar");
     const navbarRect = navbar.getBoundingClientRect();
     const aboutRect = about.getBoundingClientRect();
-    if(navbarRect.bottom >= aboutRect.top && navbarRect.bottom <= aboutRect.bottom) {
+    if (navbarRect.bottom >= aboutRect.top && navbarRect.bottom <= aboutRect.bottom) {
         navbar.classList.add("bg-2");
         navbar.classList.remove("bg-1");
     } else {
@@ -35,11 +42,11 @@ document.addEventListener("scroll", () => {
     }
 })
 hiddenElements.forEach(el => observer.observe(el));
-
+observer.observe(techStack);
 // Hamburger btn
 
 hamburgerBtn.addEventListener("click", () => {
-    if(hamburgerBtn.classList.contains("hamburger-active")) {
+    if (hamburgerBtn.classList.contains("hamburger-active")) {
         hamburgerBtn.classList.remove("hamburger-active");
         hamburgerBtn.firstElementChild.classList.remove("bar-active");
         sidebar.classList.remove("sidebar-active");
@@ -50,7 +57,7 @@ hamburgerBtn.addEventListener("click", () => {
     }
 });
 const navChildren = navbarMobile.children;
-for(const child of navChildren) {
+for (const child of navChildren) {
     child.addEventListener("click", () => {
         hamburgerBtn.classList.remove("hamburger-active");
         hamburgerBtn.firstElementChild.classList.remove("bar-active");
@@ -58,7 +65,7 @@ for(const child of navChildren) {
     })
 }
 // Copy to clipboard
-copy.forEach((element, i) => element.addEventListener("click", async () => { 
+copy.forEach((element, i) => element.addEventListener("click", async () => {
     try {
         await navigator.clipboard.writeText(text[i].innerHTML);
     } catch (err) {
